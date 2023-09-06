2fa03290691ae169e749d9ff73f42fe7f1e63dac
Thomas Valla <thomasvalla4@gmail.com>
2023-09-05 21:36:37 -0300

    Updates

--- precache-manifest.e144e35802c386a04b3a01e4848bdd0e.js
+++ precache-manifest.e144e35802c386a04b3a01e4848bdd0e.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "ec5c5ee5523cc3f11b41",
-    "url": "/gym_trainer_page/static/js/main.0e2c01c2.chunk.js"
+    "revision": "c1cdbca9b89d19e977a8",
+    "url": "/gym_trainer_page/static/js/main.c6421064.chunk.js"
   },
   {
     "revision": "c9d5aab023f66adc498f",
@@ -12,7 +12,7 @@ self.__precacheManifest = [
     "url": "/gym_trainer_page/static/js/2.8808d590.chunk.js"
   },
   {
-    "revision": "94e2e4929dfbfdd0fb4591fe19f86b48",
+    "revision": "8c44933fb7a97b84efe7cddaa6a9f671",
     "url": "/gym_trainer_page/index.html"
   }
 ];
