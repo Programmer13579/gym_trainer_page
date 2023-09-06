1ae2a28ddd85cf1babc38251f869cb51a2ed886d
Thomas Valla <thomasvalla4@gmail.com>
2023-09-05 21:27:44 -0300

    Updates

---
+++ precache-manifest.94ad1d63d714b6f23fbc0537d9e0c274.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec5c5ee5523cc3f11b41",
+    "url": "/react-proyecto/static/js/main.0e2c01c2.chunk.js"
+  },
+  {
+    "revision": "bb2397921495036b2eba",
+    "url": "/react-proyecto/static/js/runtime~main.4aaef749.js"
+  },
+  {
+    "revision": "99b1961c55d675e491f5",
+    "url": "/react-proyecto/static/js/2.8808d590.chunk.js"
+  },
+  {
+    "revision": "13f1d35ab392bf5620c020f5d2a80815",
+    "url": "/react-proyecto/index.html"
+  }
+];
