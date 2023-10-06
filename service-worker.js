7b3e089018efedf5845c6e91ece7cdc9cbba97b8
Thomas Valla <thomasvalla4@gmail.com>
2023-10-06 12:04:59 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gym_trainer_page/precache-manifest.3b662c364d86f0d9ef4fad571b4a62b2.js"
+  "/gym_trainer_page/precache-manifest.f7fc6eae0a362f5c086433553c09b3ac.js"
 );
 
 workbox.clientsClaim();
