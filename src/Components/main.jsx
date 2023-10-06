7e7fecdfc8e3d7ef7196eedba0e12150f144178d
Thomas Valla <thomasvalla4@gmail.com>
2023-10-06 12:04:34 -0300

    Completé la animación del texto del slider

--- src/Components/main.jsx
+++ src/Components/main.jsx
@@ -12,9 +12,7 @@ export const nextImage = (p_time, p_setInterval, p_animation, p_setAnimation, p_
         p_setAnimation(p_animation + 1)
     else
         p_setAnimation(0)
-    document.getElementById('slider_text').style.animation = "sliderAnimate .2s linear forwards";
-    document.getElementById('slider_text_2').style.animation = "sliderAnimate .2s linear forwards";
-    document.getElementById('slider_text_3').style.animation = "sliderAnimate .2s linear forwards";
+    sliderTextAnimation()
 }
 
 export const prevImage = (p_time, p_setInterval, p_animation, p_setAnimation, p_images) => {
@@ -23,9 +21,18 @@ export const prevImage = (p_time, p_setInterval, p_animation, p_setAnimation, p_
         p_setAnimation(p_animation - 1)
     else
         p_setAnimation(p_images.length - 1)
-    document.getElementById('slider_text').style.animation = "sliderAnimate2 .2s linear forwards";
-    document.getElementById('slider_text_2').style.animation = "sliderAnimate2 .2s linear forwards";
-    document.getElementById('slider_text_3').style.animation = "sliderAnimate2 .2s linear forwards";
+    sliderTextAnimation()
+}
+
+const sliderTextAnimation = () => {
+    document.getElementById('slider_text').classList.remove('slider_text_animation_animation');
+    document.getElementById('slider_text_2').classList.remove('slider_text_animation_animation');
+    document.getElementById('slider_text_3').classList.remove('slider_text_animation_animation');
+    setTimeout(function(){
+        document.getElementById('slider_text').classList.add('slider_text_animation_animation');
+        document.getElementById('slider_text_2').classList.add('slider_text_animation_animation');
+        document.getElementById('slider_text_3').classList.add('slider_text_animation_animation');
+    }, 200)
 }
 
 export const scrollToSection = (ref) => {
@@ -68,9 +75,9 @@ const main = ({setContacto}) => {
                 
                 <div className="container_info_trainer">
                     <div className="info_trainer">
-                        <p className='slider_text_animation' id='slider_text'>Entrenador personal</p>
-                        <h3 className='slider_text_animation' id='slider_text_2'>SDN <span>GYM</span></h3>
-                        <button className='slider_text_animation' id='slider_text_3' onClick={() => setContacto(true)}>Contactame</button>
+                        <p className='slider_text_animation slider_text_animation_animation' id='slider_text'>Entrenador personal</p>
+                        <h3 className='slider_text_animation slider_text_animation_animation' id='slider_text_2'>SDN <span>GYM</span></h3>
+                        <button className='slider_text_animation slider_text_animation_animation' id='slider_text_3' onClick={() => setContacto(true)}>Contactame</button>
                     </div>
                 </div>
             </div>
